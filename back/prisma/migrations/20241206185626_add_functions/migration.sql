48575600f821894bd6fa2534d383dff558099562
martinkupa <mkupa@fi.uba.ar>
2024-11-24 00:37:49 -0300

    Create schema and physical model

---
+++ back/prisma/migrations/20241206185626_add_functions/migration.sql
@@ -0,0 +1,28 @@
+CREATE FUNCTION "fn_IsLowercaseAlphanumerical" (string VARCHAR) RETURNS BOOLEAN LANGUAGE SQL IMMUTABLE RETURNS NULL ON NULL INPUT RETURN string NOT SIMILAR TO '%[^a-z0-9_]%';
+
+CREATE FUNCTION "fn_ManuallyCascadeDeleteUserData" () RETURNS TRIGGER AS $tg_UserData_BeforeDelete$
+DECLARE
+    user_id INTEGER;
+BEGIN
+    SELECT pk_user_id INTO user_id FROM "User" WHERE fk_username_uq = OLD.pk_username;
+
+    IF (NOT FOUND) THEN
+        RAISE EXCEPTION 'Username % does not have an associated user_id, the database is in an invalid state!', OLD.pk_username;
+    END IF;
+
+    DELETE FROM "CollectionEntry" WHERE fk_user_id = user_id;
+
+    DELETE FROM "CardPackHistoryEntry" WHERE fk_user_id = user_id;
+
+    WITH "cte_DeletedUsers" AS (
+        SELECT pk_user_id FROM "User" WHERE fk_username_uq IS NULL AND deleted_at IS NOT NULL
+    ) DELETE FROM 
+        "TradeOffer" 
+    WHERE 
+        (fk_receiver_id IN (SELECT pk_user_id FROM "cte_DeletedUsers") OR fk_receiver_id = user_id)
+    AND
+        (fk_offerer_id IN (SELECT pk_user_id FROM "cte_DeletedUsers") OR fk_offerer_id = user_id);
+      
+    RETURN OLD; -- Indicates the trigger should continue on to the next row
+END;
+$tg_UserData_BeforeDelete$ LANGUAGE plpgsql;
