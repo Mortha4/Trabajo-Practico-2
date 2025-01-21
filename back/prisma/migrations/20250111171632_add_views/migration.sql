f4e33c2d31c02d07e3997dbe6b093ed97e668a93
martinkupa <mkupa@fi.uba.ar>
2025-01-21 01:06:41 -0300

    Create initial openapi spec and implement prototype endpoints

--- back/prisma/migrations/20250111171632_add_views/migration.sql
+++ back/prisma/migrations/20250111171632_add_views/migration.sql
@@ -8,3 +8,22 @@ SELECT
     ) AS "has_expired"
 FROM
     "Trade";
+
+CREATE VIEW "vw_FlattenedUser" AS
+SELECT
+    "User".pk_user_id,
+    "User".fk_username_uq,
+    "User".deleted_at,
+    "UserData".profile_name,
+    "UserData".email_uq,
+    COALESCE(
+        "UserData".profile_picture_path,
+        'public/deleted-profile-picture.svg'
+    ) AS profile_picture_path,
+    "UserData"."password",
+    "UserData".privilege,
+    "UserData".created_at,
+    "UserData".modified_at
+FROM
+    "User"
+    LEFT JOIN "UserData" ON "User".fk_username_uq = "UserData".pk_username;
---
+++ back/src/prisma-errors.ts
@@ -0,0 +1,5 @@
+enum PrismaError {
+    UNIQUE_CONSTRAINT_VIOLATION = "P2002",
+}
+
+export default PrismaError;
