bb76734e4e4db8ac67ddfadae04d20418f760dcf
Mariano <margerich@fi.uba.ar>
2025-02-03 14:26:13 -0300

    added first set of cards and first cardpack

--- back/prisma/seed.mjs
+++ back/prisma/seed.mjs
@@ -1,45 +1,147 @@
-import { PrismaClient } from "@prisma/client";
+import { CardSeason, PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 const rarity = await prisma.rarity.createMany({
     data: [
         { name: "Common", dropProbability: 1 },
-        { name: "Rare", dropProbability: 0.333 },
+        { name: "Rare", dropProbability: 0.6 },
+        { name: "Epic", dropProbability: 0.3 },
+        { name: "Legendary", dropProbability: 0.1 }
     ],
 });
 
-const cardTest1 = await prisma.cardClass.createMany({
+const cards = await prisma.cardClass.createMany({
     data: [
         {
-            name: "carta1",
-            title: "Carta de prueaba 1",
-            season: "Season1",
-            description: "Esta es la carta de prueba 1",
-            rarity: "Common",
-        },
-        {
-            name: "carta2",
-            title: "Carta de prueaba 2",
-            season: "Season1",
-            description: "Esta es la carta de prueba 2",
-            rarity: "Common",
-        },
-        {
-            name: "carta3",
-            title: "Carta de prueaba 3",
-            season: "Season1",
-            description: "Esta es la carta de prueba 3",
+            name: "walter_white",
+            title: "Walter White",
+            season: CardSeason.Season1,
+            description: "Walter Hartwell White,  es un profesor de química de Albuquerque, que, tras ser diagnosticado con cáncer de pulmón inoperable, decide empezar a fabricar metanfetamina para poder mantener a su familia.",
             rarity: "Rare",
         },
+        {
+            name: "jesse_pinkman",
+            title: "Jessee Pinkman",
+            season: CardSeason.Season1,
+            description: "En el instituto, Jesse era un consumidor, fabricante y traficante de poca monta de metanfetamina. Era un estudiante que prestaba poca atención a las clases de química de su profesor, el propio Walter.",
+            rarity: "Rare",
+        },
+        {
+            name: "skyler_white",
+            title: "Skyler White",
+            season: CardSeason.Season1,
+            description: "Skyler White (de soltera Lambert) es la mujer de Walter White y madre de Walter White Jr. y Holly White. Skyler ha tenido múltiples trabajos o actividades para obtener dinero. ",
+            rarity: "Rare"
+        },
+        {
+            name: "walter_jr",
+            title: "Walter Jr.",
+            season: CardSeason.Season1,
+            description: "Walter Hartwell White Jr., también conocido por el apodo 'Flynn' es el hijo de Walter y Skyler White y el hermano de Holly White. Estudia en el mismo instituto en el que su padre imparte clases. ",
+            rarity: "Rare"
+        },
+        {
+            name: "hank_schrander",
+            title: "Henry 'Hank' Schrander",
+            season: CardSeason.Season1,
+            description: "Henry R. 'Hank' Schrader es el marido de Marie Schrader (hermana de Skyler White) y agente especial a cargo de las oficinas de la DEA en Albuquerque. ",
+            rarity: "Rare"
+        },
+        {
+            name: "marie_schrander",
+            title: "Marie Schrander",
+            season: CardSeason.Season1,
+            description: "Marie Schrader (de soltera Lambert) es la hermana de Skyler White y mujer del agente de la DEA Hank Schrader. ",
+            rarity: "Rare"
+        },
+        {
+            name: "steve_gomez",
+            title: "Steve Gomez",
+            season: CardSeason.Season1,
+            description: "fue el compañero de Hank en las oficinas de la DEA de Albuquerque y un buen amigo suyo. Apodado 'Gomie' por Hank, a menudo colabora en la investigación de Heisenberg y el cristal azul. ",
+            rarity: "Common"
+        },
+        {
+            name: "carmen_molina",
+            title: "Carmen Molina",
+            season: CardSeason.Season1,
+            description: "Carmen Molina es la directora del instituto J.P. Wynne, donde Walter es profesor de química. ",
+            rarity: "Common"
+        },
+        {
+            name: "krazy_8",
+            title: "Loco-8",
+            season: CardSeason.Season1,
+            description: "Domingo Gallardo Molina (alias 'Loco-8') es un distribuidor de metanfetamina previamente asociado con Jesse Pinkman y Emilio Koyama. ",
+            rarity: "Common"
+        },
+        {
+            name: "tuco_salamanca",
+            title: "Tuco Salamanca",
+            season: CardSeason.Season1,
+            description: "Tuco Salamanca es un traficante de drogas mexicano que actuó brevemente como distribuidor de metanfetamina para Walter White y Jesse Pinkman. ",
+            rarity: "Rare"
+        },
+        {
+            name: "gretchen_schwartz",
+            title: "Gretchen Schwartz",
+            season: CardSeason.Season1,
+            description: "Gretchen Schwartz es la antigua compañera de química de Walter y co-fundadora de Gray Matter Technologies. Tuvo una relación amorosa con Walter y actualmente está casada con Elliott Schwartz. ",
+            rarity: "Common"
+        },
+        {
+            name: "skinny_pete",
+            title: "Flaco Pete",
+            season: CardSeason.Season1,
+            description: "Peter 'Flaco Pete' es uno de los mejores amigos de Jesse Pinkman. Pasó algún tiempo en la cárcel, donde conoció a Tuco Salamanca, a quien luego presenta a Jesse para que hagan negocios. ",
+            rarity: "Common"
+        },
+        {
+            name: "badger",
+            title: "Badger",
+            season: CardSeason.Season1,
+            description: "Brandon 'Badger' Mayhew es uno de los tres amigos de Jesse que acaban haciendo negocios con él. Posteriormente, acaba trabajando en la venta del producto de Walter y Jesse. ",
+            rarity: "Common"
+        },
+        {
+            name: "combo",
+            title: "Combo",
+            season: CardSeason.Season1,
+            description: "Christian 'Combo' Ortega es uno de los tres amigos de Jesse que trabajaron para él en el negocio del tráfico de su metanfetamina. ",
+            rarity: "Common"
+        },
+        {
+            name: "laboratory",
+            title: "Camioneta de Heisenberg",
+            season: CardSeason.Season1,
+            description: "Jesse we need to cook",
+            rarity: "Epic"
+        }
     ],
 });
 
-const testPack = await prisma.cardPackType.create({
+const season1Pack = await prisma.cardPackType.create({
     data: {
-        name: "paquete_prueba",
-        title: "Paquete de prueba",
+        name: "season_1_pack",
+        title: "Paquete Temporada 1",
         drops: {
-            create: [{ cardName: "carta1" }, { cardName: "carta2" }],
+            create: [
+                { cardName: "walter_white" },
+                { cardName: "jesse_pinkman" },
+                { cardName: "skyler_white" },
+                { cardName: "walter_jr" },
+                { cardName: "hank_schrander" },
+                { cardName: "marie_schrander" },
+                { cardName: "steve_gomez" },
+                { cardName: "carmen_molina" },
+                { cardName: "krazy_8" },
+                { cardName: "tuco_salamanca" },
+                { cardName: "gretchen_schwartz" },
+                { cardName: "skinny_pete" },
+                { cardName: "badger" },
+                { cardName: "combo" },
+                { cardName: "laboratory" }
+            ],
         },
     },
 });
@@ -49,7 +151,7 @@ const testPackEpic = await prisma.cardPackType.create({
         name: "paquete_prueba_epico",
         title: "Paquete de prueba epico",
         drops: {
-            create: [{ cardName: "carta3" }],
+            create: [{ cardName: "combo" }],
         },
     },
 });
@@ -61,38 +163,41 @@ const user = await prisma.user.create({
                 username: "admin",
                 profileName: "Perfil de prueba",
                 email: "prueba@gmail.com",
-                password: "admin",
+                password: "password",
                 privilege: "Administrator",
             },
         },
         collection: {
             create: [
-                {
-                    cardName: "carta1",
-                    quantity: 4,
-                },
-                {
-                    cardName: "carta2",
-                    quantity: 1,
-                },
-                {
-                    cardName: "carta3",
-                    quantity: 2,
-                },
+                { cardName: "walter_white", quantity: 1 },
+                { cardName: "jesse_pinkman", quantity: 1 },
+                { cardName: "skyler_white", quantity: 1 },
+                { cardName: "walter_jr", quantity: 1 },
+                { cardName: "hank_schrander", quantity: 1 },
+                { cardName: "marie_schrander", quantity: 1 },
+                { cardName: "steve_gomez", quantity: 1 },
+                { cardName: "carmen_molina", quantity: 1 },
+                { cardName: "krazy_8", quantity: 1 },
+                { cardName: "tuco_salamanca", quantity: 1 },
+                { cardName: "gretchen_schwartz", quantity: 1 },
+                { cardName: "skinny_pete", quantity: 1 },
+                { cardName: "badger", quantity: 1 },
+                { cardName: "combo", quantity: 1 },
+                { cardName: "laboratory", quantity: 1 },
             ],
         },
         packOpening: {
             create: [
                 {
-                    packName: "paquete_prueba",
+                    packName: "season_1_pack",
                     details: {
                         create: [
                             {
-                                cardName: "carta1",
-                                quantity: 4,
+                                cardName: "combo",
+                                quantity: 1,
                             },
                             {
-                                cardName: "carta2",
+                                cardName: "badger",
                                 quantity: 1,
                             },
                         ],
@@ -116,20 +221,20 @@ const user2 = await prisma.user.create({
         collection: {
             create: [
                 {
-                    cardName: "carta3",
-                    quantity: 5,
+                    cardName: "combo",
+                    quantity: 1,
                 },
             ],
         },
         packOpening: {
             create: [
                 {
-                    packName: "paquete_prueba_epico",
+                    packName: "season_1_pack",
                     details: {
                         create: [
                             {
-                                cardName: "carta3",
-                                quantity: 5,
+                                cardName: "combo",
+                                quantity: 1,
                             },
                         ],
                     },
